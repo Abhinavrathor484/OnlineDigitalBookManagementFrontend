930fe34041dda2f1a965aab35f7d8da0aea3cf96
Akshaya6767 <Akshaya.Shree@cognizant.com>
2025-04-04 18:17:20 +0530

    added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  // Add more routes here if needed
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
