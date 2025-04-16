6ea4fd8b3f4c949af1e5922a5c2983e1afb06994
Ravi Sharma <2387787@cognizant.com>
2025-04-16 11:07:52 +0530

    11:07 am commit 16th April

--- src/app/AuthGuard.ts
+++ src/app/AuthGuard.ts
@@ -1,33 +1,3 @@
-// import { Injectable } from '@angular/core';
-// import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
-// import { jwtDecode } from 'jwt-decode';
-
-// @Injectable({
-//   providedIn: 'root'
-// })
-// export class AdminGuard implements CanActivate {
-
-//   constructor(private router: Router) {}
-
-//   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
-//     const token = localStorage.getItem('authToken');
-//     if (token) {
-//       const decoded: any = jwtDecode(token);
-//       const role = decoded['http://schemas.microsoft.com/ws/2008/06/identity/claims/role'];
-//       console.log(role);
-//       if (role === 'Admin') {
-//         return true;
-//       } else {
-//         alert("Only Admins can access the resource!");
-//         this.router.navigate(['/User-Login-Page']);
-//         return false;
-//       }
-//     } else {
-//       return false;
-//     }
-//   }
-// }
-
 import { Injectable } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
 
